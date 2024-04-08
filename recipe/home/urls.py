4db239755ede335ef5902a4ea5f547afd6a2123d
amaanlakdawala <amaanlakdawala301@gmail.com>
2024-04-08 15:23:39 +0530

    authentication system added login and logout page created

--- recipe/home/urls.py
+++ recipe/home/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('',success,name="home"),
     path('delete/<pk>/',delete,name="delete"),
      path('update/<pk>/',update,name="update"),
+     path('login_page/',login_page,name='login_page'),
+     path('register/',register, name='register'),
+     path('logout_page/',logout_page,name='logout_page'),
 
 ]
--- recipe/home/views.py
+++ recipe/home/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import *
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
-
+@login_required(login_url='login_page')
 def success(request):
    
     if (request.method == 'POST'):
@@ -50,7 +54,56 @@ def update(request,pk):
         return render(request,'home/update.html',context)
         
              
-             
+def login_page(request):
+        if request.method=="POST":
+          username=request.POST.get('username')
+          password = request.POST.get('password')
+          
+          if not User.objects.filter(username=username).exists():
+                messages.error(request, "invalid username.")
+                return redirect('login_page')
+          user = authenticate(username=username, password=password)
+          if user is None:
+               messages.error(request, "Invalid password")
+               return redirect('login_page')
+          else:
+               messages.success(request, "success password")
+               login(request,user)
+               return redirect('home')
+        return render(request,'home/login.html')
+        
+               
+                   
+       
+def logout_page(request):
+     logout(request)
+     return redirect('login_page')
+
+
+
+def register(request):
+     
+     if request.method=="POST":
+          username=request.POST.get('username')
+          password = request.POST.get('password')
+
+          user=User.objects.filter(username=username)
+          if user.exists():
+               messages.error(request, " User already exists.")
+               return redirect('register')
+          
+
+          user =User.objects.create(
+               username=username,
+          )
+          user.set_password(password)
+          user.save()
+          messages.success(request, " registered successfully.")
+          return redirect('register')
+         
+
+
+     return render(request,'home/register.html')           
        
         
 
